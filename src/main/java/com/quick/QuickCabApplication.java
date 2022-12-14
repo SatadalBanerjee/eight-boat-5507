c72a4d05126bca4b3a98702d3ce80aceb146a02b
Satadal Banerjee <satadalbanerjeebiz@gmail.com>
2022-12-14 12:24:59 +0530

    adding main foler

---
+++ src/main/java/com/quick/QuickCabApplication.java
@@ -0,0 +1,13 @@
+package com.quick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickCabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quick/QuickCabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
